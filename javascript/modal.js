7415d6e3fd2708717cce207f96c3b2e288793a5b
Pitarcus <pitarcusmp@gmail.com>
2024-01-23 15:43:17 +0100

    Model live preview UX
    Better UX with a progress bar, nicer feeling buttons and layout.

--- javascript/modal.js
+++ javascript/modal.js
@@ -12,6 +12,8 @@ var sandModel = document.getElementById("substance-model-sand");
 var jaraModel = document.getElementById("substance-model-jaraflower");
 var bridgeModel = document.getElementById("substance-model-bridge");
 
+let models = [dirtModel, snowModel, sandModel, jaraModel, bridgeModel];
+
 // Get the <span> element that closes the modal
 var closeButton = document.getElementsByClassName("close")[0];
 
@@ -34,90 +36,128 @@ window.onclick = function(event) {
   }
 }
 
-
+// -- MODEL SELECTION BUTTONS --
 // User clicks one of the model selection buttons
 dirtModel.onclick = function() {
-    modelViewer.src = "./assets/models/Dirt_ground.glb"
+  modelViewer.src = "./assets/models/Dirt_ground.glb"
 }
 snowModel.onclick = function() {
-    modelViewer.src = "./assets/models/Snow_ground.glb"
+  modelViewer.src = "./assets/models/Snow_ground.glb"
 }
 sandModel.onclick = function() {
-    modelViewer.src = "./assets/models/Sand_ground.glb"
+  modelViewer.src = "./assets/models/Sand_ground.glb"
 }
 jaraModel.onclick = function() {
-    modelViewer.src = "./assets/models/Jara_flower.glb"
+  modelViewer.src = "./assets/models/Jara_flower.glb"
 }
 bridgeModel.onclick = function() {
   modelViewer.src = "./assets/models/Bridge.glb"
 }
 
+models.forEach(element => {
+  element.addEventListener("click", setModelViewer)
+  element.addEventListener("click", SwitchActiveModelButton)
+    
+});
+
+function SwitchActiveModelButton(event) {
+  if(!event.target.classList.contains(".active")){
+    var currentActive = modal.querySelector(".active");
+    if(currentActive != null) {
+      currentActive.classList.remove("active");
+    }
+
+    event.target.classList.add("active");
+  }
+}
+
+function setModelViewer() {
+  modelViewer.dismissPoster();
+}
+
 
 // Let the user control the environment
-  let lastX;
-  let panning = false;
-  let skyboxAngle = 0;
-  let radiansPerPixel;
-      
-  const startPan = () => {
-    const orbit = modelViewer.getCameraOrbit();
-    const { radius } = orbit;
-    radiansPerPixel = -1 * radius / modelViewer.getBoundingClientRect().height;
-    modelViewer.interactionPrompt = 'none';
-  };
-  
-  const updatePan = (thisX) => {      
-    const delta = (thisX - lastX) * radiansPerPixel;
-    lastX = thisX;
-    skyboxAngle += delta;
-    const orbit = modelViewer.getCameraOrbit();
-    orbit.theta += delta;
-    modelViewer.cameraOrbit = orbit.toString();
-    modelViewer.resetTurntableRotation(skyboxAngle);
-    modelViewer.jumpCameraToGoal();
+let lastX;
+let panning = false;
+let skyboxAngle = 0;
+let radiansPerPixel;
+    
+const startPan = () => {
+  const orbit = modelViewer.getCameraOrbit();
+  const { radius } = orbit;
+  radiansPerPixel = -1 * radius / modelViewer.getBoundingClientRect().height;
+  modelViewer.interactionPrompt = 'none';
+};
+
+const updatePan = (thisX) => {      
+  const delta = (thisX - lastX) * radiansPerPixel;
+  lastX = thisX;
+  skyboxAngle += delta;
+  const orbit = modelViewer.getCameraOrbit();
+  orbit.theta += delta;
+  modelViewer.cameraOrbit = orbit.toString();
+  modelViewer.resetTurntableRotation(skyboxAngle);
+  modelViewer.jumpCameraToGoal();
+}
+
+modelViewer.addEventListener('mousedown', (event) => {
+  panning = event.button === 2 || event.ctrlKey || event.metaKey || event.shiftKey;
+  if (!panning)
+    return;
+
+  lastX = event.clientX;
+  startPan();
+  event.stopPropagation();
+}, true);
+
+modelViewer.addEventListener('touchstart', (event) => {
+  const {targetTouches, touches} = event;
+  panning = targetTouches.length === 2 && targetTouches.length === touches.length;
+  if (!panning)
+    return;
+
+  lastX = 0.5 * (targetTouches[0].clientX + targetTouches[1].clientX);
+  startPan();
+}, true);
+
+self.addEventListener('mousemove', (event) => {
+  if (!panning)
+    return;
+
+  updatePan(event.clientX);
+  event.stopPropagation();
+}, true);
+
+modelViewer.addEventListener('touchmove', (event) => {
+  if (!panning || event.targetTouches.length !== 2)
+    return;
+
+  const {targetTouches} = event;
+  const thisX = 0.5 * (targetTouches[0].clientX + targetTouches[1].clientX);
+  updatePan(thisX);
+}, true);
+
+self.addEventListener('mouseup', (event) => {
+  panning = false;
+}, true);
+
+modelViewer.addEventListener('touchend', (event) => {
+  panning = false;
+}, true);
+
+
+
+// Progress Bar logic
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.model-preview-progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+    updatingBar.style.width = `${0}%`;
+  } else {
+    progressBar.classList.remove('hide');
   }
+};
+modelViewer.addEventListener('progress', onProgress);
   
-  modelViewer.addEventListener('mousedown', (event) => {
-    panning = event.button === 2 || event.ctrlKey || event.metaKey || event.shiftKey;
-    if (!panning)
-      return;
-
-    lastX = event.clientX;
-    startPan();
-    event.stopPropagation();
-  }, true);
-
-  modelViewer.addEventListener('touchstart', (event) => {
-    const {targetTouches, touches} = event;
-    panning = targetTouches.length === 2 && targetTouches.length === touches.length;
-    if (!panning)
-      return;
-
-    lastX = 0.5 * (targetTouches[0].clientX + targetTouches[1].clientX);
-    startPan();
-  }, true);
-
-  self.addEventListener('mousemove', (event) => {
-    if (!panning)
-      return;
-
-    updatePan(event.clientX);
-    event.stopPropagation();
-  }, true);
-
-  modelViewer.addEventListener('touchmove', (event) => {
-    if (!panning || event.targetTouches.length !== 2)
-      return;
-
-    const {targetTouches} = event;
-    const thisX = 0.5 * (targetTouches[0].clientX + targetTouches[1].clientX);
-    updatePan(thisX);
-  }, true);
-
-  self.addEventListener('mouseup', (event) => {
-    panning = false;
-  }, true);
-  
-  modelViewer.addEventListener('touchend', (event) => {
-    panning = false;
-  }, true);
